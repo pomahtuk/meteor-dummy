3ac2ae706bc4bff25cb008d941514d89814cb701
pomahtuk <pman89@ya.ru>
2015-08-01 21:27:37 +0200

    wip on prototype

---
+++ client/templates/botanika-pages.js
@@ -0,0 +1,16 @@
+Template.botanikaBody.onRendered(function () {
+  var slideout = new Slideout({
+    'panel': document.getElementById('main'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+    slideout.toggle();
+  });
+
+  document.querySelector('.menu').addEventListener('click', function(eve) {
+    if (eve.target.nodeName === 'A') { slideout.close(); }
+  });
+});
---
+++ client/templates/main-structure.js
@@ -0,0 +1,27 @@
+Template.mainStructure.onRendered(function () {
+  var swiperH = new Swiper('.swiper-container-h', {
+    pagination: '.swiper-pagination-h',
+    paginationClickable: true,
+    spaceBetween: 10,
+    keyboardControl: true,
+    grabCursor: true,
+    threshold: 100,
+    mousewheelControl: true,
+    mousewheelForceToAxis: true,
+    speed: 500
+  });
+
+  var swiperV = new Swiper('.swiper-container-v', {
+    speed: 500,
+    pagination: '.swiper-pagination-v',
+    paginationClickable: true,
+    direction: 'vertical',
+    spaceBetween: 10,
+    keyboardControl: true,
+    mousewheelControl: true,
+    grabCursor: true,
+    hashnav: true,
+    threshold: 100,
+    mousewheelForceToAxis: true
+  });
+});
