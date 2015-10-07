5bd4956211ed114319bae3c4e3f936a9242a1db6
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-10-07 13:37:47 +0200

    index page progress

--- client/templates/botanika.js
+++ client/templates/botanika.js
@@ -2,7 +2,7 @@ Template.botanikaBody.onRendered(function () {
   var slideout = new Slideout({
     'panel': document.getElementById('main'),
     'menu': document.getElementById('menu'),
-    'padding': 256,
+    'padding': -256,
     'tolerance': 70
   });
 
