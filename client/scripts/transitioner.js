fa9f9cd6bd58b9f870df1eb1b2959a283a3282f5
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-11-09 16:28:21 +0100

    video bg player code + some fixes + cleanups

--- client/scripts/transitioner.js
+++ client/scripts/transitioner.js
@@ -1,58 +1,7 @@
-let animationInDuration = 1000;
-let animationOutDuration = 1600;
-
-$.Velocity.RegisterEffect('transition.slideDown', {
-  defaultDuration: animationInDuration,
-  calls: [[ {translateY: ['0%', '-100%'], translateZ: 0, easing: 'easeOutCubic', opacity: [1, 1]} ]]
-});
-
-$.Velocity.RegisterEffect('transition.slideDownOut', {
-  defaultDuration: animationOutDuration,
-  calls: [[ {translateY: ['100%', '0%'], translateZ: 0, easing: 'linear', opacity: [1, 1]} ]]
-});
-
-$.Velocity.RegisterEffect('transition.slideUpOut', {
-  defaultDuration: animationOutDuration,
-  calls: [[ {translateY: ['-100%', '0%'], translateZ: 0, easing: 'linear', opacity: [1, 1]} ]]
-});
-
-$.Velocity.RegisterEffect('transition.slideUp', {
-  defaultDuration: animationInDuration,
-  calls: [[ {translateY: ['0%', '100%'], translateZ: 0, easing: 'easeOutCubic', opacity: [1, 1]} ]]
-});
+let animationInDuration = 1000,
+  animationOutDuration = 1600;
 
 Transitioner.default({
   in: ['fadeIn', {duration: animationInDuration, display: 'flex'}],
   out: ['fadeOut', {duration: animationInDuration }]
 });
-// 
-// function registerDownTransition(params) {
-//   Transitioner.transition({
-//     fromRoute: params.fromRoute,
-//     toRoute: params.toRoute,
-//     velocityAnimation: {in: 'transition.slideDown', out: 'transition.slideDownOut'}
-//   });
-// }
-//
-// function registerUpTransition(params) {
-//   Transitioner.transition({
-//     fromRoute: params.fromRoute,
-//     toRoute: params.toRoute,
-//     velocityAnimation: {in: 'transition.slideUp', out: 'transition.slideUpOut'}
-//   });
-// }
-//
-// // rougth representation of menu structure
-// var routesArray = ['main', 'houses', 'infrastructure', 'evolution', 'lab', 'contacts']
-//
-// routesArray.forEach((route, index) => {
-//   // starting from next one
-//   let routesLeftover = routesArray.slice(index);
-//   if (routesLeftover.length > 0) {
-//     // we are not at the last route
-//     routesLeftover.forEach((nextRoute) => {
-//       registerUpTransition({fromRoute: route, toRoute: nextRoute});
-//       registerDownTransition({fromRoute: nextRoute, toRoute: route})
-//     });
-//   }
-// });
--- packages/botanika-admin/lib/client/js/admin_layout.js
+++ packages/botanika-admin/lib/client/js/admin_layout.js
@@ -16,30 +16,6 @@ Template.AdminLayout.destroyed = function () {
   $('body').removeClass('fixed');
 };
 
-var backGroudTypeChecker = function () {
-  var $typeSelect = $('.cover-type-select'),
-    $photoInput = $('.cover-type-photo').parents('.form-group'),
-    $videoUrlInput = $('.cover-type-video').parents('.form-group');
-
-  $typeSelect.change(function () {
-    var selectVal = $typeSelect.val();
-
-    if (selectVal === 'true') {
-      $photoInput.hide();
-      $videoUrlInput.show();
-    } else if (selectVal === 'false') {
-      $photoInput.show();
-      $videoUrlInput.hide();
-    } else {
-      $photoInput.hide();
-      $videoUrlInput.hide();
-    }
-  }).change();
-};
-
-Template.AdminDashboardEdit.onRendered(backGroudTypeChecker);
-Template.AdminDashboardNew.onRendered(backGroudTypeChecker);
-
 Template.AdminLayout.helpers({
   minHeight: function () {
     return Template.instance().minHeight.get() + 'px'
