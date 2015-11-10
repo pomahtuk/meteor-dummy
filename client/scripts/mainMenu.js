9ad34e10a8f6c60309ebc0f5c52b0f3a8be8fa03
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-11-10 16:36:15 +0100

    navigation fixed

--- client/scripts/mainMenu.js
+++ client/scripts/mainMenu.js
@@ -1,9 +1,15 @@
 Template.mainMenu.onRendered(function () {
   $('.menu-item .menu-item-link, .menu-to-home').click(function (evt) {
     let $elem = $(this),
-      index = $elem.data('index');
-    if(index || index === 0) {
+      index = 0,
+      slug = $elem.attr('href');
+
+    // cut off a hash
+    slug = slug.split('#')[1];
+
+    if (slug) {
       evt.preventDefault();
+      index = $(`[data-hash=${slug}]`).index();
     }
 
     Meteor.swiperV.slideTo(index);
